9182e95b961973a86a0c293c2af849357ac8fffc
alizaynoune <simo.jokir@gmail.com>
2020-11-14 20:50:23 +0100

    add d05[ex01 => ex03]
    Signed-off-by: alizaynoune <simo.jokir@gmail.com>

---
+++ d05/ex01/ex01.sql
@@ -0,0 +1,1 @@
+CREATE TABLE IF NOT EXISTS ft_table (`id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, `login` VARCHAR(8) DEFAULT 'toto', `group` ENUM('staff', 'student', 'other') NOT NULL, `creation_date` DATE NOT NULL);
